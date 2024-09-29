8617a597cbaaf297e2c0f35bcc272c63f9e7ab3e
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-09-29 19:04:56 +0000

    fix: Application of styling with regards to leading and trailing whitespace

--- tools/api-markdown-documenter/src/documentation-domain-to-markdown/default-transformations/PlainTextToMarkdown.ts
+++ tools/api-markdown-documenter/src/documentation-domain-to-markdown/default-transformations/PlainTextToMarkdown.ts
@@ -18,12 +18,68 @@ export function plainTextToMarkdown(
 	node: PlainTextNode,
 	context: TransformationContext,
 ): MdastTree {
-	// TODO: verify escaped-ness
-	return applyFormatting(
+	// TODO: don't escape text that's already escaped.
+
+	const anyFormatting =
+		context.bold === true || context.italic === true || context.strikethrough === true;
+
+	if (!anyFormatting) {
+		return applyFormatting(
+			{
+				type: "text",
+				value: node.value,
+			},
+			context,
+		);
+	}
+
+	// We will render leading and trailing whitespace *outside* of any formatting to prevent potential issues.
+	const { leadingWhitespace, body, trailingWhitespace } = splitLeadingAndTrailingWhitespace(
+		node.value,
+	);
+
+	const result: MdastTree = [];
+
+	if (leadingWhitespace) {
+		result.push({
+			type: "text",
+			value: leadingWhitespace,
+		});
+	}
+
+	const formattedBody = applyFormatting(
 		{
 			type: "text",
-			value: node.text,
+			value: body,
 		},
 		context,
 	);
+	result.push(formattedBody);
+
+	if (trailingWhitespace) {
+		result.push({
+			type: "text",
+			value: trailingWhitespace,
+		});
+	}
+
+	return result;
+}
+
+interface SplitTextResult {
+	leadingWhitespace: string;
+	body: string;
+	trailingWhitespace: string;
+}
+
+function splitLeadingAndTrailingWhitespace(text: string): SplitTextResult {
+	// split out the [ leading whitespace, body, trailing whitespace ]
+	const [, leadingWhitespace, body, trailingWhitespace]: string[] =
+		text.match(/^(\s*)(.*?)(\s*)$/) ?? [];
+
+	return {
+		leadingWhitespace,
+		body,
+		trailingWhitespace,
+	};
 }
